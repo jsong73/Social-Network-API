d013718e1f78a4e9c784a6e487046a63480f931e
jsong73 <jessicajinsong@gmail.com>
2022-11-14 23:36:43 -0500

    created server.js file

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const db = require("./config/connection");
+
+const { User , Thought, Reaction } = require("./models");
+
+const PORT = process.env.PORT || 3005;
+const app = express();
+
+db.once("open", () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}`);
+    });
+});
