f449d68e2799e9e98ac01d8e77e0718bd687caa5
jsong73 <jessicajinsong@gmail.com>
2022-11-15 23:59:18 -0500

    created routes folder

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
-// const Reaction = require("./Reaction");
-// const Thought = require("./Thought");
+const Reaction = require("./Reaction");
+const Thought = require("./Thought");
 const User = require("./User")
 
-module.exports ={User}
-//  { User, Thought, Reaction };
+module.exports = { User, Thought, Reaction };
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const routes = require("./routes")
 const db = require("./config/connection");
-
-
-const { User , Thought, Reaction } = require("./models");
 
 const PORT = process.env.PORT || 3003;
 const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(routes);
+
+
 
 db.once("open", () => {
     app.listen(PORT, () => {
