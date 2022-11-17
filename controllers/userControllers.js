3afe830043d01f363ef361d853b64696cd1591e7
jsong73 <jessicajinsong@gmail.com>
2022-11-17 01:18:48 -0500

    finished user controllers

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -53,6 +53,7 @@ module.exports = {
                 $set: req.body,
                 },
                 {
+                //validate user ID
                 runValidators: true,
                 new: true,
                 }
@@ -63,5 +64,55 @@ module.exports = {
                 }
                 res.json(userData);
             })
-        }
-}
+                .catch((err) => {
+                    console.log(err);
+                    res.status(500).json(err)
+                });
+        },
+
+        deleteUser(req, res) {
+            User.findOneAndDelete({ _id: req.params.userId })
+            .then((userData) =>{
+                if(!userData) {
+                    return res.status(404).json({ msg: "No user with this ID!"})
+                }
+                //will delete all users ids thoughts
+                return Thought.deleteMany({ _id: {$in: userData.thoughts }});
+            })
+                .then(()=> {
+                    res.json({ msg: "User and thoughts have been deleted!"})
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.status(500).json(err)
+                });
+        },
+        //ADD (POST) friend
+        addFriend(req, res) {
+            User.findOneAndUpdate({ _id: req.params.userId}, { $addToSet: {friends: req.params.friendId}}, {new: true})
+                .then((userData) => {
+                    if (!userData){
+                        return res.status(404).jsong({ msg: "No user with this ID!"});
+                    }
+                    res.json({msg: "Friend added!", userData});
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.status(500).json(err);
+                });
+        },
+        // DELETE friend from users friend list
+        deleteFriend(req, res) {
+            User.findOneAndUpdate({ _id: req.params.userId}, {$pull: { friends: req.params.friendId }}, { new: true })
+                .then((userData) => {
+                    if(!userData) {
+                        return res.status(404).json({ msg: "No user with this ID!"});
+                    }
+                    res.json({ msg: "Friend deleted!", userData});
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.status(500).json(err);
+                });
+        },
+};
